110598eec6169d84bcabf45af796b7bc82a2dd63
Jamie Terry <jnterry@ntlworld.com>
2017-07-29 14:40:46 +0100

    Add all.js test file which will run others, update package.json to just run that file for npm test

---
+++ test/all.js
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
+///                       Part of any-db-q                               ///
+////////////////////////////////////////////////////////////////////////////
+/// \file 02-simple-query.js
+/// \author Jamie Terry
+/// \date 2017/07/29
+/// \brief Tests for running simple queries against the database
+////////////////////////////////////////////////////////////////////////////
+
+"use strict";
+
+function importTest(name, path){
+	if(path == null){ path = name; }
+
+	describe(name, function(){
+		require("./" + path);
+	});
+}
+
+describe('AnyDbQ', () => {
+	importTest('connect');
+});
