d652ebb26d67601825e3eb7988662fefe428ccd4
Jamie Terry <jnterry@ntlworld.com>
2017-07-29 14:23:43 +0100

    Add tests for bad connection parameters

--- test/01-connect.js
+++ test/01-connect.js
@@ -21,6 +21,18 @@ describe('AnyDbQ', () => {
 			return connection;
 		}
 
+		function connectFails(done, options){
+			AnyDbQ(options)
+				.then((connection) => {
+					done(new Error("Execution shouldn't get here; " +
+								   "a connection was returned")
+						);
+				}).fail((error) => {
+					expect(error).to.exist;
+					done();
+				});
+		}
+
 		it('Single connection returns valid connection', () => {
 			return AnyDbQ({
 				'adapter'  : 'sqlite3',
@@ -32,6 +44,29 @@ describe('AnyDbQ', () => {
 				'adapter' : 'sqlite3',
 				'pool'     : { min : 2, max : 32 }
 			}).then(isValidConnection);
+		});
+
+		it('Invalid adapter value returns invalid connection', (done) => {
+			return connectFails(done, {'adapter' : 'fake'});
+		});
+
+		it('Bad credentials return invalid connection', (done) => {
+			return connectFails(done, {
+				'adapter'   : 'mysql',
+				'host'      : 'localhost',
+				'user'      : 'X_BAD_USER_X',
+				'password'  : '_A_PASSWORD_'
+			});
+		});
+
+		it('Bad port return invalid connection', (done) => {
+			return connectFails(done, {
+				'adapter'   : 'mysql',
+				'host'      : 'localhost',
+				'user'      : 'X_BAD_USER_X',
+				'password'  : '_A_PASSWORD_',
+				'port'      : '-1'
+			});
 		});
 	});
 });
