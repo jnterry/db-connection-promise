08b448136b17157ec836b2976740fdaf3470153a
Jamie Terry <jnterry@ntlworld.com>
2018-01-03 00:07:37 +0000

    Update transaction wrapper tests to use new chain syntax

--- test/transaction-wrapper.js
+++ test/transaction-wrapper.js
@@ -12,18 +12,19 @@
 require('./common.js');
 
 it('Insert in transaction and commit', () => {
-	return initUserTable().then((dbh) => {
-		return dbh.transaction((dbh) => {
-			return dbh.query(`INSERT INTO user (id, username, password) VALUES
-			                      (1, 'bob', 'pass');`
-			                )
+	return initUserTable()
+		.transaction((dbh) => {
+			return dbh
+				.query(`INSERT INTO user (id, username, password) VALUES
+			                           (1, 'bob', 'pass');`
+				      )
 				.then((results) => {
 					expect(results             ).does.exist;
 					expect(results.rowCount    ).to.deep.equal(1);
 					expect(results.rows        ).to.deep.equal([]);
-
-					return dbh.query(`SELECT * FROM user;`)
-				}).then((results) => {
+				})
+				.query(`SELECT * FROM user;`)
+				.then((results) => {
 					// Check data is visible within the transaction
 					expect(results             ).does.exist;
 					expect(results.lastInsertId).is.not.ok;
@@ -35,9 +36,9 @@ it('Insert in transaction and commit', () => {
 						password : 'pass',
 					});
 				});
-		}).then(() => {
-			return dbh.query(`SELECT * FROM user;`);
-		}).then((results) => {
+		})
+		.query(`SELECT * FROM user;`)
+		.then((results) => {
 			// No error should have occurred within transaction, so
 			// results should have been committed and be visible outside
 			// the transaction
@@ -51,23 +52,22 @@ it('Insert in transaction and commit', () => {
 				password : 'pass',
 			});
 		});
-	});
 });
 
 it('Insert in transaction and rollback', () => {
-	return initUserTable().then((dbh) => {
-		return dbh.transaction((dbh) => {
-			return dbh.query(`INSERT INTO user (id, username, password) VALUES
-			                      (1, 'bob', 'pass');`
-			                )
+	return initUserTable()
+		.transaction((dbh) => {
+			return dbh
+				.query(`INSERT INTO user (id, username, password) VALUES
+			                           (1, 'bob', 'pass');`
+				      )
 				.then((results) => {
 					expect(results             ).does.exist;
 					expect(results.rowCount    ).to.deep.equal(1);
 					expect(results.rows        ).to.deep.equal([]);
-
-					return dbh.query(`SELECT * FROM user;`)
-				}).then((results) => {
-
+				})
+				.query(`SELECT * FROM user;`)
+				.then((results) => {
 					// Check data is visible within the transaction
 					expect(results             ).does.exist;
 					expect(results.lastInsertId).is.not.ok;
@@ -81,87 +81,95 @@ it('Insert in transaction and rollback', () => {
 
 					throw "A bad thing happened";
 				});
-		}).then(() => {
-			return dbh.query(`SELECT * FROM user;`);
-		}).then((results) => {
-			// No error should have occurred within transaction, so
-			// results should have been commited and be visable outside
-			// the transaction
+		})
+		.query(`SELECT * FROM user;`)
+		.then((results) => {
+			// Error occurred within the transaction, so it should have been
+			// rolled back, and results will not be visible
 			expect(results             ).does.exist;
 			expect(results.lastInsertId).is.not.ok;
 			expect(results.rowCount    ).to.deep.equal(0);
 			expect(results.rows        ).to.deep.equal([]);
 		});
-	});
 });
 
 it('Nested Transactions - Commit All', () => {
-	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'}).then((dbh) => {
-		return dbh.transaction((dbh) => {
-			return dbh.query(`UPDATE user SET username = 'me' WHERE id = 9`)
-				.then((results) => {
-					return dbh.transaction((dbh) => {
-						return dbh.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`));
-					});
+	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'})
+		.transaction((dbh) => {
+			return dbh
+				.query(`UPDATE user SET username = 'me' WHERE id = 9`)
+				.transaction((dbh) => {
+					return dbh.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`));
 				});
-		}).then(() => {
-			// By now both transactions should have been committed
-
-			return dbh.query(`SELECT * FROM user ORDER BY id ASC`)
-				.then((results) => {
-					expect(results             ).does.exist;
-					expect(results.lastInsertId).is.not.ok;
-					expect(results.rowCount    ).to.deep.equal(2);
-					expect(results.rows[0]     ).to.deep.equal({ id: 5, username: 'a',  password: 'b'   });
-					expect(results.rows[1]     ).to.deep.equal({ id: 9, username: 'me', password: 'root'});
-				});
+		}) // By now both transactions should have been committed
+		.query(`SELECT * FROM user ORDER BY id ASC`)
+		.then((results) => {
+			expect(results             ).does.exist;
+			expect(results.lastInsertId).is.not.ok;
+			expect(results.rowCount    ).to.deep.equal(2);
+			expect(results.rows[0]     ).to.deep.equal({ id: 5, username: 'a',  password: 'b'   });
+			expect(results.rows[1]     ).to.deep.equal({ id: 9, username: 'me', password: 'root'});
 		});
-	});
 });
 
 it('Nested Transactions - Inner Rollback', () => {
-	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'}).then((dbh) => {
-		return dbh.transaction((dbh) => {
-			return dbh.query(`UPDATE user SET username = 'me' WHERE id = 9`)
-				.then((results) => {
-					return dbh.transaction((dbh) => {
-						return dbh.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`))
-							.then(() => { throw "A nasty error :o"; });
-					});
+	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'})
+		.transaction((dbh) => {
+			return dbh
+				.query(`UPDATE user SET username = 'me' WHERE id = 9`)
+				.transaction((dbh) => {
+					return dbh
+						.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`))
+						.then(() => { throw "A nasty error :o"; });
 				});
-		}).then(() => {
-			// By now both transactions should have been committed
-
-			return dbh.query(`SELECT * FROM user ORDER BY id ASC`)
-				.then((results) => {
-					expect(results             ).does.exist;
-					expect(results.lastInsertId).is.not.ok;
-					expect(results.rowCount    ).to.deep.equal(1);
-					expect(results.rows[0]     ).to.deep.equal({ id: 9, username: 'me', password: 'root'});
-				});
+		})
+		.query(`SELECT * FROM user ORDER BY id ASC`)
+		.then((results) => {
+			// By now inner transaction should have been rolled back, but outer committed
+			expect(results             ).does.exist;
+			expect(results.lastInsertId).is.not.ok;
+			expect(results.rowCount    ).to.deep.equal(1);
+			expect(results.rows[0]     ).to.deep.equal({ id: 9, username: 'me', password: 'root'});
 		});
-	});
 });
 
 it('Nested Transactions - Outer Rollback', () => {
-	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'}).then((dbh) => {
-		return dbh.transaction((dbh) => {
-			return dbh.query(`UPDATE user SET username = 'me' WHERE id = 9`)
-				.then((results) => {
-					return dbh.transaction((dbh) => {
-						return dbh.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`));
-					});
-				}).then(() => { throw "A nasty error :o"; });
-		}).then(() => {
-			// By now both transactions should have been committed
-
-			return dbh.query(`SELECT * FROM user ORDER BY id ASC`)
-				.then((results) => {
-					expect(results             ).does.exist;
-					expect(results.lastInsertId).is.not.ok;
-					expect(results.rowCount    ).to.deep.equal(1);
-					expect(results.rows[0]     ).to.deep.equal({ id: 9, username: 'admin', password: 'root'});
-				});
+	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'})
+		.transaction((dbh) => {
+			return dbh
+				.query(`UPDATE user SET username = 'me' WHERE id = 9`)
+				.transaction((dbh) => {
+					return dbh.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`));
+				})
+				.then(() => { throw "A nasty error :o"; });
+		}).query(`SELECT * FROM user ORDER BY id ASC`)
+		.then((results) => {
+			// By now the outer transaction should have been rolled back, which will
+			// implicitly roll back the inner transaction
+			expect(results             ).does.exist;
+			expect(results.lastInsertId).is.not.ok;
+			expect(results.rowCount    ).to.deep.equal(1);
+			expect(results.rows[0]     ).to.deep.equal({ id: 9, username: 'admin', password: 'root'});
 		});
-	});
+});
+
+it('Nested Transactions - Both Rollback', () => {
+	return initUserTableWithUser({ id: 9, username: 'admin', password: 'root'})
+		.transaction((dbh) => {
+			return dbh
+				.query(`UPDATE user SET username = 'me' WHERE id = 9`)
+				.transaction((dbh) => {
+					return dbh
+						.query((`INSERT INTO user (id, username, password) VALUES (5, 'a', 'b')`))
+						.then(() => { throw "A nasty error :o"; });
+				})
+				.then(() => { throw "An even nastier error :o"; });
+		}).query(`SELECT * FROM user ORDER BY id ASC`)
+		.then((results) => {
+			// By now both transactions should have been rolled back
+			expect(results             ).does.exist;
+			expect(results.lastInsertId).is.not.ok;
+			expect(results.rowCount    ).to.deep.equal(1);
+			expect(results.rows[0]     ).to.deep.equal({ id: 9, username: 'admin', password: 'root'});
+		});
 });
